543c10a3efdcd86cb4a3c966477c9cb4eec758c9
Sophie Li <limisoph@gmail.com>
2021-03-27 03:06:25 -0400

    initial firewall visualizations

--- scripts/verify_db.py
+++ scripts/verify_db.py
@@ -8,9 +8,18 @@ def main():
     cur = conn.cursor()
     # cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
     # print(cur.fetchall())
-    t = ("Teardown", )
-    cur.execute("SELECT * FROM data \
-        WHERE Operation=?", t)
+    # t = ("Teardown", )
+    # cur.execute("SELECT * FROM data \
+    #     WHERE Operation=?", t)
+    # print(cur.fetchone())
+    # cur.execute("PRAGMA table_info(data);")
+    # print(cur.fetchall())
+
+    cur.execute("SELECT DISTINCT `Syslog Priority` \
+         FROM data;")
+    print(cur.fetchall())
+    cur.execute("SELECT COUNT(*) FROM data \
+        WHERE `Syslog Priority`=?", ('Critical',))
     print(cur.fetchone())
     cur.close()
     conn.close()
